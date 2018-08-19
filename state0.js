59ba71e2a697ebc4d8b639febbcc71f06634157f
edgardojs <edgardoj.santiago@gmail.com>
2018-08-18 21:02:54 -0400

    Added Scenes

--- state0.js
+++ state0.js
@@ -1,17 +1,38 @@
-var demo = {};
+var demo = {},centerX=1500/2,centerY=1000/2,char,speed=6;
 demo.state0 = function(){};
 demo.state0.prototype = {
-  preload:function(){},
+  preload:function(){
+    game.load.image('char','assets/char/char.png',32,32);
+    game.load.spritesheet('orc','assets/badGuysPNG/Orc/orc_regular_bald.png',32,32);
+
+  },
   create:function(){
     game.stage.backgroundColor = "#D0DFD0";
     console.log('state0');
-
-
     addChangeStateEventListeners();
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    char=game.add.sprite(centerX,centerY,'char');
+    orc=game.add.sprite(10,1000,'orc');
+    char.anchor.setTo(0.5,0.5);
+    orc.anchor.setTo(0.2,0.2)
+
 
   },
-  update:function(){}
+  update:function(){
+    if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+      char.x += speed;
+    }
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+      char.x -= speed;
+
+    }
+    if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+      char.y -= speed;
+    }
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+      char.y += speed;
+    }
+  }
 };
 function changeState(i,stateNum){
 
